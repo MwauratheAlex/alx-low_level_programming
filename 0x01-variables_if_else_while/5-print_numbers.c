b14fe7e7a92e5a511cb0bea0934d91fe7d02c7a8
mwaurathealex <mwaurathealex@gmail.com>
2022-09-09 09:29:07 -0400

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+
+	return (0);
+}
