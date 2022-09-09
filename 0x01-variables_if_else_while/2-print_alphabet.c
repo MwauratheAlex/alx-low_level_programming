566138be7b0e058f5b1df159edaa060d56e81bc7
mwaurathealex <mwaurathealex@gmail.com>
2022-09-09 09:03:39 -0400

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function prints the alphabet in lower case
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
