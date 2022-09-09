e63e7f24a305a1582d53e3e506da10dfd37977a9
mwaurathealex <mwaurathealex@gmail.com>
2022-09-09 09:52:38 -0400

    add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
