7d6c17b118744d4bf9a202a1bc41325312d2ff9c
mwaurathealex <mwaurathealex@gmail.com>
2022-09-16 19:32:22 +0300

    edit1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,16 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i,j;
 
 	for (i = 0; i < 10; i++)
 	{
-		int j;
-
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 14; j++)
 		{
 			if (j > 9)
 				_putchar((j % 10) + '0');
-			_putchar((j % 10) + '0);
+			_putchar((j %10) + '0');
 		}
 	}
-
-	_putchar('\n');
 }
 
