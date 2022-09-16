0d790bdbfbc76028f8ffb6783e703d1e47a91225
mwaurathealex <mwaurathealex@gmail.com>
2022-09-16 20:07:02 +0300

    add print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a digonal line
+ *
+ * @n: number of times the character '\' is printed
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j, k;
+
+		k = 0;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < k; j++)
+				_putchar(' ');
+			_putchar('\\');
+			
+			k++;
+		}
+	}
+	_putchar('\n');
+}
+
