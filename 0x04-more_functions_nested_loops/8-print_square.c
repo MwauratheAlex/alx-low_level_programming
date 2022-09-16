342662a11ce4ffddabd3de0528b3f8c8b12444d8
mwaurathealex <mwaurathealex@gmail.com>
2022-09-16 20:32:27 +0300

    add print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ *
+ * @size: size of the square
+ *
+ * Return:0
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
