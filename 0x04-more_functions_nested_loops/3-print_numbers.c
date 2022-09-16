20f33266062aa37e7bb33422ec73e42bcb621f9f
mwaurathealex <mwaurathealex@gmail.com>
2022-09-16 17:33:52 +0300

    add print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 1 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
