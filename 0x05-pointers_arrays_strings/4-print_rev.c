06f46d8dd674868a1bd005549203893b8e16b37b
mwaurathealex <mwaurathealex@gmail.com>
2022-09-19 20:22:25 +0300

    ...

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,11 @@
  */
 void print_rev(char *s)
 {
-	
 	int i, c = 0;
-	
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		c++;
-	}
 
+	for (i = 0; *(s + i) != '\0'; i++)
+		c++;
 	for (i = c - 1; i >= 0; i--)
 		_putchar (*(s + i));
-
 	_putchar ('\n');
 }
