860e11e4045f67795af6b09020e60acfea23d8b1
mwaurathealex <mwaurathealex@gmail.com>
2022-09-20 10:24:39 +0300

    ..

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src != '\0')
-		*dest++ = *src++;
+	int i;
 
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	*(dest) = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
