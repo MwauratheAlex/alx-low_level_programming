29ba3aac20f3edc62b509768e170c992fbaa93a8
mwaurathealex <mwaurathealex@gmail.com>
2022-09-20 09:34:47 +0300

    ..

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,5 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-	{
-		(i != n - 1) ? printf("%d, ", a[i]) : printf("%d", a[i]);
-	}
+		(i != n - 1) ? printf("%d, ", a[i]) : printf("%d\n", a[i]);
 }
