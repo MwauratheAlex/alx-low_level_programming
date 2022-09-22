f4f80f07f5a51270db53e93dedff2eae23eaf5ce
mwaurathealex <mwaurathealex@gmail.com>
2022-09-22 07:27:27 +0300

    add 3-strcpy.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,17 @@
  *
  * @s2: pointer to second string
  *
- * Return: 1 if equal, 0 if not equal
+ * Return: 0 if s1 == s2, -1 if s1 < s2, 1 if 21 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (0);
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (s1[i] - s2[i]);
 }
