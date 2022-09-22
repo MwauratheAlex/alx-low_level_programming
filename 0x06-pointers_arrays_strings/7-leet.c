c1faba88e86b6e8455e947bfa8bcb26a0c7d069f
mwaurathealex <mwaurathealex@gmail.com>
2022-09-22 15:22:56 +0300

    add 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,11 +3,34 @@
 /**
  * leet - encodes a string into 1337
  *
- * @targ: pointer to the string
+ * @str: pointer to the string
  *
  * Return: pointer to encoded string
  */
-char *leet(char *)
+char *leet(char *str)
 {
-	return (*);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		switch (str[i])
+		{
+			case 'a': case 'A':
+				str[i] = '4';
+				break;
+			case 'e': case 'E':
+				str[i] = '3';
+				break;
+			case 'o': case 'O':
+				str[i] = '0';
+				break;
+			case 't': case 'T':
+				str[i] = '7';
+				break;
+			case 'l': case 'L':
+				str[i] = '1';
+				break;
+		}
+	}
+	return (str);
 }
