0d6d140358c5d553e8da6a2692f541447e0da8b4
mwaurathealex <mwaurathealex@gmail.com>
2022-09-22 16:26:08 +0300

    figured out the issue

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,10 @@ char *cap_string(char *str)
 		while (sep[j] != '\0')
 		{
 			if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] -= 32;
+			{
+				if (str[i + 1] != '\0')
+					str[i + 1] -= 32;
+			}
 			j++;
 		}
 		i++;
