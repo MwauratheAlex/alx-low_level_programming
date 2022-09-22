59428896a59373f71d4cf06a8b37dc93f67b2bcf
mwaurathealex <mwaurathealex@gmail.com>
2022-09-22 14:03:21 +0300

    ...

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,8 +16,8 @@ char *cap_string(char *str)
 	while (str[i] != '\0')
 	{
 		j = 0;
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
+		if (str[i + 1] >= 'A' && str[i + 1] <= 'Z')
+			str[i + 1] += 32;
 
 		while (sep[j] != '\0')
 		{
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -9,6 +9,8 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
+		if (str[i+1] >= 'A' && str[i+1] <= 'Z')
+			str[i+1] += 32;
 		j = 0;
 		while(sep[j] != '\0')
 		{
