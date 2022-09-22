f3246aa2b0f726928a4dc38a80b979037fca276b
mwaurathealex <mwaurathealex@gmail.com>
2022-09-22 06:45:01 +0300

    .

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,13 +13,23 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
-	for(i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[i] = '\0';
+		i++;
 	}
+		j = 0;
+	while (src[j] != '\0')
+	{
+		if (j >= n)
+			break;
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
