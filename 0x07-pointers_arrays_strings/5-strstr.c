1b5c449f3e5e5023c4e6b6ddd50d9242deffc919
mwaurathealex <mwaurathealex@gmail.com>
2022-09-26 15:52:21 +0300

    add 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,5 +10,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i, j;
+
+	for (i = 0; *(haystack + i) != '\0'; i++)
+	{
+		for (j = 0; *(needle + j) != '\0'; j++)
+		{
+			if (haystack[i + j] == needle[j])
+				continue;
+			break;
+		}
+		if (*(needle + j) == '\0')
+			return (haystack + i);
+	}
+
 	return (0);
 }
