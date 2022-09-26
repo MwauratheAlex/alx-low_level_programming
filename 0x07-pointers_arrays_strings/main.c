44a4df4c5d4a0e5cc1cf0c338e16f74ec51df483
mwaurathealex <mwaurathealex@gmail.com>
2022-09-26 13:57:38 +0300

    add 1-memcpy.c

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-	char buffer[98] = {0x00};
+	char buffer[98] = {0};
+	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
 	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
+	_memcpy(buffer + 50, buffer2, 10);
 	printf("-------------------------------------------------\n");
 	simple_print_buffer(buffer, 98);
 	return (0);
