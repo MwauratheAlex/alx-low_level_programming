cbfbd159ef6fa57deddcdad5c651c0894749d933
mwaurathealex <mwaurathealex@gmail.com>
2022-09-26 14:08:40 +0300

    edit 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '/0')
+	while (*s != '\0')
 	{
 		if (*s++ == c)
 			return (s);
 	}
-	return (NULL);
+	return (s);
 }
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	char buffer[98] = {0};
-	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+	char *s = "hello";
+	char *f;
+	f = _strchr(s, 'l');
 
-	simple_print_buffer(buffer, 98);
-	_memcpy(buffer + 50, buffer2, 10);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
 	return (0);
 }
 
