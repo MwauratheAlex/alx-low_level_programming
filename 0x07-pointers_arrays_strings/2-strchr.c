0c349c7366718a43bea09227734a35006d47f678
mwaurathealex <mwaurathealex@gmail.com>
2022-09-26 14:17:59 +0300

    edit 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,8 +11,9 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s++ == c)
-			return (s - 1);
+		if (*s == c)
+			return (s);
+		++s;
 	}
-	return (s);
+	return (0);
 }
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char *s = "hello";
 	char *f;
-	f = _strchr(s, 'l');
+	f = _strchr(s, 'o');
 
 	if (f != NULL)
 	{
