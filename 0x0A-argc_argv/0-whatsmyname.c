78bf52559dcf6737d0439055e8bbaf3428efe24e
mwaurathealex <mwaurathealex@gmail.com>
2022-09-30 10:22:13 +0300

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints it's own name followed by a new line
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}
