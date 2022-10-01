0aa44520a9591f6b77077627113ac360e903b940
mwaurathealex <mwaurathealex@gmail.com>
2022-10-01 09:32:07 +0300

    add 6-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,22 +2,39 @@
 #include <stdlib.h>
 
 /**
- * main - adds positive numbers
+ * main - adds positive number
  *
- * @argc: arguement count
+ * @argc: argument count
  *
- * @argv: arguement vector
+ * @argv: arguent vector
  *
- * Return: 0
+ * Return: 0 (Success), or 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int sum, num, i, j;
+
+	sum = 0;
 
 	for (i = 1; i < argc; i++)
-		sum += atoi(argv[i]);
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+		if (num >= 0)
+			sum += num;
+	}
 
 	printf("%d\n", sum);
-
 	return (0);
 }
