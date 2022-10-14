e8778c7b2b4d792300ecc8605c153fdd3638ad78
MwauratheAlex <mwaurathealex@gmail.com>
2022-10-14 17:30:33 +0300

    edit task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i;
 
-	if(nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	p = malloc(nmemb * size);
@@ -26,4 +26,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		p[i] = 0;
 
 	return (p);
-}	
+}
