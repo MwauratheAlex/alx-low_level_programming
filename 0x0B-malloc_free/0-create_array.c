af0613ddd0af337b64928fea3b1e4d753bacabd0
MwauratheAlex <mwaurathealex@gmail.com>
2022-10-14 07:47:23 +0300

    edit task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,9 @@ char *create_array(unsigned int size, char c)
 	char *p = malloc(sizeof(char) * size);
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	return (p);
