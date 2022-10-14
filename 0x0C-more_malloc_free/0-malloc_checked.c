034507e31ae3492e246a034d36febae33383fc44
MwauratheAlex <mwaurathealex@gmail.com>
2022-10-14 12:36:14 +0300

    add task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: size of memory
+ *
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	return (malloc(b));
+}
