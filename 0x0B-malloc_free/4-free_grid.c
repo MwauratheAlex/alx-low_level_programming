47d969a7828150a233427886b3f6f3782538ed54
MwauratheAlex <mwaurathealex@gmail.com>
2022-10-14 11:40:13 +0300

    edit task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,7 +14,7 @@ void free_grid(int **grid, int height)
 	if (grid == NULL || height == 0)
 		return;
 
-	for (; height <= 0; height--)
+	for (; height >= 0; height--)
 		free(grid[height]);
 	free(grid);
 }
