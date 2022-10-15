d9705fd187cce5d221b94c41f5e751c4c56b9ad3
MwauratheAlex <mwaurathealex@gmail.com>
2022-10-15 10:22:13 +0300

    add task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from, followed by a new line
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line
  *
  * Return: nothing
  */
