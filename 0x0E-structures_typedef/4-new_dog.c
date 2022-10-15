f78cc82dba9f80ef38b2b0b694e002941a8be900
MwauratheAlex <mwaurathealex@gmail.com>
2022-10-15 16:05:29 +0300

    add task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,22 @@
 #include "dog.h"
+#include <stdlib.h>
 
+/**
+ * str_len - find the lenth of a string
+ *
+ * @s: string
+ *
+ * Return: length of s
+ */
+int str_len(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (len);
+}
 /**
  * new_dog - creates a new dog
  *
@@ -14,9 +31,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int len1, len2;
 
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
-	
+
+	len1 = str_len(name);
+	len2 = str_len(owner);
+
+	dog->name = malloc((len1 + 1) * sizeof(char));
+
+	if (dog->name == NULL)
+		return (NULL);
+
+	dog->owner = malloc((len2 + 1) * sizeof(char));
+
+	if (dog->owner == NULL)
+		return (NULL);
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}
