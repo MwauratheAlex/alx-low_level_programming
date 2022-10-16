bf2da883a62a638a1d306a976bd8f9da67f07243
MwauratheAlex <mwaurathealex@gmail.com>
2022-10-16 11:04:20 +0300

    add task 2

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, NULL, "Django");
+    return (0);
+}
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,1 +1,39 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings, followed by a new line
+ *
+ * @separator: the string to be printed between the strings
+ *
+ * @n: the number of strings passed to the function
+ *
+ * @...: arguements to the function
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	if (separator == NULL)
+		separator = "";
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			str = "(nil)";
+		(i != n - 1) ?
+			printf("%s%s", str, separator) :
+			printf("%s", str);
+	}
+
+	printf("\n");
+	va_end(args);
+}
