ed1e39caad58e88ecc04d45dc502cbc691079715
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-09 05:29:05 +0300

    add task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list -  frees a list_t list
+ *
+ * @head: first node in the list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (!head)
+		return;
+
+	while (head)
+	{
+		list_t *current;
+
+		current = head;
+		free(current->str);
+		free(current);
+		head = head->next;
+	}
+}
+
