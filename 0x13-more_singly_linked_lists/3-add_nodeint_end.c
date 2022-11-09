80991e887e5499df5002e950b2b7ca026704f735
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-09 09:59:46 +0300

    edit task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the end of a listint_t list
+ * add_nodeint_end - adds a new node at the end of a listint_t list
  *
  * @head: ponter to the first node
  *
