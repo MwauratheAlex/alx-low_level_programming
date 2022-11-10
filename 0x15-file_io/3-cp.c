2f824d7a27d7a09d96cafc8d1773b405a9e821e3
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-10 15:39:07 +0300

    edit task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -135,7 +135,7 @@ int main(int argc, char **argv)
 			len_w = -1;
 		if (len_w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
