439b6423aa434dcf70f5fd7ceaaa7daa107a164b
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-11 08:51:42 +0300

    edit task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz ");
-		else if (i % 5 == 0 && i % 3 != 0) 
+		else if (i % 5 == 0 && i % 3 != 0)
 			(i == 100) ? printf("Buzz\n") : printf("Buzz ");
 		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
