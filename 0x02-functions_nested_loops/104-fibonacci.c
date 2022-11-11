8017ff30caad383a467fee096e6220e032c81702
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-11 07:45:25 +0300

    edit task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,47 +8,17 @@
  */
 int main(void)
 {
-	long i = 1;
-	long j = 2;
-	long k = i + j;
-	long i_1, i_2, j_1, j_2, k_1, k_2;
-	int count = 3;
-	int first_time = 1;
+	unsigned long i = 1;
+	unsigned long j = 2;
+	unsigned long k = i + j;
+	unsigned int count = 3;
+
+	unsigned long i_1, i_2, j_1, j_2, k_1, k_2;
 
 	printf("%lu, %lu, ", i, j);
 
-	while (count < 98)
+	while (count < 93)
 	{
-		if (count >= 93)
-		{
-			if (first_time == 1)
-			{
-				i_1 = i / 10000000000;
-				j_1 = j / 10000000000;
-				i_2 = i % 10000000000;
-				j_2 = j % 10000000000;
-				first_time = 0;
-			}
-
-			k_1 = i_1 + j_1;
-			k_2 = i_2 + j_2;
-
-			if (k_2 > 9999999999)
-			{
-				k_1 += 1;
-				k_2 %= 10000000000;
-			}	
-			printf("%lu%lu, ", k_1, k_2);
-			
-			i_1 = j_1;
-			i_2 = j_2;
-			j_1 = k_1;
-			j_2 = k_2;
-			count++;
-
-			continue;
-		}
-		
 		printf("%lu, ", k);
 		i = j;
 		j = k;
@@ -56,10 +26,36 @@ int main(void)
 		count++;
 	}
 
-	printf("%lu%lu\n", (k / 10000000000), (j % 10000000000));
+	i_1 = i / 10000000000;
+	i_2 = i % 10000000000;
 
+	j_1 = j / 10000000000;
+	j_2 = j % 10000000000;
 
+	for (count = 93; count < 99; count++)
+	{
+		k_1 = i_1 + j_1;
+		k_2 = i_2 + j_2;
 
+		if ((i_2 + j_2) > 9999999999)
+		{
+			k_1 += 1;
+			k_2 %= 10000000000;
+		}
+
+		printf("%lu%lu", k_1, k_2);
+		if (count != 98)
+			printf(", ");
+
+		i_1 = j_1;
+		i_2 = j_2;
+		j_1 = k_1;
+		j_2 = k_2;
+	}
+
+	printf("\n");
+
+	
 	return (0);
 }
 
