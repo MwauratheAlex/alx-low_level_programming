5c89bc2f402d16b09fe5b22d413588160a1dc7c4
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-11 12:06:58 +0300

    add task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		putchar(45);
+		i = -i;
+	}
+
+	if (i / 10)
+		print_number(i / 10);
+
+	putchar(i % 10 + '0');
+}
+
