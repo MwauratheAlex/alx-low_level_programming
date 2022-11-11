a701a594933b9d7692cc4c6ce63fbcede6740132
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-11 12:29:56 +0300

    edit task 101

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -9,13 +9,5 @@ int main(void)
 {
     print_number(98);
     _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
     return (0);
 }
