703f463326b9476e3c2d4f17b2e544d18e8bc764
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-11 12:12:35 +0300

    edit task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,13 +13,11 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		i = -i;
 	}
-
 	if (i / 10)
 		print_number(i / 10);
 
-	putchar(i % 10 + '0');
+	_putchar(i % 10 + '0');
 }
-
