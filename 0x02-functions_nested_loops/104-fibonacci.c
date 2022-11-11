cab2759493828e800ade8ffef7ad504880fb837b
MwauratheAlex <mwaurathealex@gmail.com>
2022-11-11 08:21:16 +0300

    edit task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line
+ * fibonnachi_98 - print first 98 fibonnachi characters
  *
- * Return: 0
+ * Return: nothing
  */
-int main(void)
+void fibonnachi_98()
 {
 	unsigned long i = 1;
 	unsigned long j = 2;
 	unsigned long k = i + j;
 	unsigned int count = 3;
-
-	unsigned long i_1, i_2, j_1, j_2, k_1, k_2;
 
 	printf("%lu, %lu, ", i, j);
 
@@ -25,7 +22,22 @@ int main(void)
 		k = i + j;
 		count++;
 	}
+	fibonnachi_93_98(i, j, count);
+}
 
+/**
+ * fibonnachi_93_98 - prints fibonnachi numbers form 93->98
+ *
+ * @i: 91st fibonnachi number
+ *
+ * @j: 92nd fibonnachi number
+ *
+ * Return: nothing
+ */
+
+void fibonnachi_93_98(int i, int j, int count)
+{
+	unsigned long i_1, i_2, j_1, j_2, k_1, k_2;
 	i_1 = i / 10000000000;
 	i_2 = i % 10000000000;
 
@@ -52,7 +64,17 @@ int main(void)
 		j_1 = k_1;
 		j_2 = k_2;
 	}
+}
 
+/**
+ * main - prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	fibonnachi_98();
 	printf("\n");
 
 	
