d98fb44d5618e325313356b229ea3f3358b7732e
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-21 22:04:37 +0300

    add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,27 +16,42 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current_node, *next_node, *new_node;
+	dlistint_t *new_node, *current_node = NULL;
 	unsigned int i;
+
+	if (*h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 
 	current_node = *h;
 
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx; i++)
 	{
-		current_node = current_node->next;
-		if (current_node == NULL && i < idx - 1)
+		if (n > (int)idx - 1)
 			return (NULL);
+		if (current_node->next == NULL)
+		{
+			new_node->prev = current_node;
+			current_node->next = new_node;
+			return (new_node);
+		}
+		current_node = current_node->next;
 	}
-
-	next_node = current_node->next;
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-	current_node->next = new_node;
-	new_node->prev = current_node;
-	new_node->n = n;
-	new_node->next = next_node;
+	if (current_node->prev == NULL)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
+	
+	new_node->next = current_node;
+	new_node->prev = current_node->prev;
+	current_node->prev->next = new_node;
+	current_node->prev = new_node;
 
 	return (new_node);
 }
--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -13,11 +13,14 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    add_dnodeint_end(&head, 0);
-    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 6);
+    add_dnodeint_end(&head, 9);
+    add_dnodeint_end(&head, 22);
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 0, 4096);
+    insert_dnodeint_at_index(&head, 7, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
