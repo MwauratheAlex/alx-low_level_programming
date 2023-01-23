188b2d2d28d552b1f9ca336d8bebe138f1c4203b
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-23 10:20:49 +0300

    add task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -25,20 +25,26 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if (!node_at_idx)
 		return (-1);
-	if (node_at_idx->prev)
+	if (node_at_idx->prev && node_at_idx->next)
 	{
 		node_at_idx->prev->next = node_at_idx->next;
 		node_at_idx->next->prev = node_at_idx->prev;
 		node_at_idx->prev = NULL;
 	}
+	
 	else if (node_at_idx->next)
 	{
 		(*head) = node_at_idx->next;
 		node_at_idx->next->prev = NULL;
 		node_at_idx->next = NULL;
 	}
-	else
+	else if (!node_at_idx->next && !node_at_idx->prev)
 		(*head) = NULL;
+	else
+	{
+		node_at_idx->prev->next = NULL;
+		node_at_idx->prev = NULL;
+	}
 	free(node_at_idx);
 	return (1);
 }
