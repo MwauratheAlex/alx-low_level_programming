26b87f19a592587a03e930caf730ac355999d167
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-05 09:28:22 +0300

    add task 5

--- 0x00-hello_world/backup/5-printf.c
+++ 0x00-hello_world/backup/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * main -  prints exactly with proper grammar,
- * but the outcome is a piece of art,
- * followed by a new line
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
