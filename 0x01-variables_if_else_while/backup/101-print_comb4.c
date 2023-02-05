0402206143737bf2f11d64b995dfd12f670946a1
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-05 11:13:33 +0300

    add task 101

--- 0x01-variables_if_else_while/backup/101-print_comb4.c
+++ 0x01-variables_if_else_while/backup/101-print_comb4.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
@@ -29,5 +29,6 @@ int main(void)
 			}
 		}
 	}
+
 	return (0);
 }
