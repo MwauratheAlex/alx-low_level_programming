385a11f64b7827ef3f033f38823a8af800a06746
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-05 11:25:35 +0300

    add task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,10 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
-
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
@@ -23,7 +22,7 @@ int main(void)
 			if (i == 98 && j == 99)
 			{
 				putchar('\n');
-				return (0);
+				break;
 			}
 			putchar(',');
 			putchar(' ');
