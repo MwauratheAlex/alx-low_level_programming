a01dbea083259a1f16f00f138ba2a03babcdc411
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-05 11:07:00 +0300

    add task 100

--- 0x01-variables_if_else_while/backup/100-print_comb3.c
+++ 0x01-variables_if_else_while/backup/100-print_comb3.c
@@ -9,13 +9,12 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-
 			if (i == 8 && j == 9)
 			{
 				putchar('\n');
@@ -25,5 +24,7 @@ int main(void)
 			putchar(' ');
 		}
 	}
+
 	return (0);
 }
+
