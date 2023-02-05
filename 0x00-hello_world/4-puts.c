7cb471b2403596c63f3045337498aec030cd893f
MwauratheAlex <mwaurathealex@gmail.com>
2023-02-05 09:24:33 +0300

    add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,");
-	puts("\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
