6d2f622b2125fd9210daa1c709c23e7f5d641567
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-05 14:18:52 +0300

    add 100-is_palindrome.c correct for betty

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,17 @@
 #include <string.h>
 
+/**
+ * is_palindrome_wrapper - wrapper around the function is_palindrome
+ * checks if s is a palindrome
+ *
+ * @s: string
+ *
+ * @len: length of the string
+ *
+ * @i: integer used to keep track of where we are
+ *
+ * Return: 1 if s is a palindrome, 0 if not
+ */
 int is_palindrome_wrapper(char *s, int len, int i)
 {
 	if (s[i] != s[(len - 1) - i])
@@ -23,7 +35,7 @@ int is_palindrome(char *s)
 	int len = strlen(s);
 
 	if (len == 0)
-		return(1);
+		return (1);
 
 	return (is_palindrome_wrapper(s, len, 0));
 }
