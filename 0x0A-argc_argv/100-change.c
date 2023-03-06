0248fbb5abf22fe774bcabcd37cec085119c2328
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-06 08:16:11 +0300

    add 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, change;
+	int cents, no_of_coins = 0;
 
 	if (argc != 2)
 	{
@@ -22,57 +22,38 @@ int main(int argc, char *argv[])
 
 	cents = atoi(argv[1]);
 
-	change = get_change(cents, 0);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-	printf("%d\n", change);
+	no_of_coins += get_no_of_coins(&cents, 25);
+	no_of_coins += get_no_of_coins(&cents, 10);
+	no_of_coins += get_no_of_coins(&cents, 5);
+	no_of_coins += get_no_of_coins(&cents, 2);
+	no_of_coins += cents;
+
+	printf("%d\n", no_of_coins);
 
 	return (0);
 }
+
 /**
- * get_change - returns minimum no of coins to make change
+ * get_no_of_coins - calculates number of coins required for change
  *
- * @cents: amount
+ * @cents: full amount
  *
- * @change: number of coins
+ * @coin: coin value available for change
  *
- * Return: minimum number of coins to make change
+ * Return: number of coins
  */
-int get_change(int cents, int change)
+int get_no_of_coins(int *cents, int coin)
 {
-	if (cents <= 0)
-		return (change);
 	int no_of_coins = 0;
 
-	if (cents >= 25)
-		call_get_change(cents, change, no_of_coins, 25);
-	else if (cents >= 10)
-		call_get_change(cents, change, no_of_coins, 10);
-	else if (cents >= 5)
-		call_get_change(cents, change, no_of_coins, 5);
-	else if (cents >= 2)
-		call_get_change(cents, change, no_of_coins, 2);
-	else if (cents == 1)
-		call_get_change(cents, change, no_of_coins, 1);
-}
+	no_of_coins += *cents / coin;
+	*cents = *cents - (no_of_coins * coin);
 
-/**
- * call_get_change - calls the function get_change
- *
- * @cents: amount
- *
- * @change: number of coins
- *
- * @no_of_coins: number of coins
- *
- * @coin_value: value of coin
- *
- * Return: nothing
- */
-void call_get_change(int cents, int change, int no_of_coins, int coin_value)
-{
-	no_of_coins += cents / coin_value;
-	change += no_of_coins;
-	cents -= no_of_coins * coin_value;
-
-	get_change(cents, change);
+	return (no_of_coins);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int get_change(int cents, int change);
+int get_no_of_coins(int *cents, int coin);
 void call_get_change(int cents, int change, int no_of_coins, int coin_value);
 void print_str(char *s);
 
