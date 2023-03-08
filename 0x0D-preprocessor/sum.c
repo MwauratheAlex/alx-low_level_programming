6c3bf3a20384d972a9b70e1073f59fb63a8f40e8
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-08 15:10:40 +0300

    you talking about practice?

---
+++ 0x0D-preprocessor/sum.c
@@ -0,0 +1,10 @@
+#include "sum.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("17 + 54 = %d\n", SUM(17, 54));
+	printf("file: %s\n", __FILE__);
+	printf("|90|: %d\n", ABS(90));
+	return (0);
+}
---
+++ 0x0D-preprocessor/sum.h
@@ -0,0 +1,7 @@
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(a, b) ((a) + (b))
+#define ABS(x) ((x) >= (0) ? (x) : (-x))
+
+#endif /* SUM_H */
