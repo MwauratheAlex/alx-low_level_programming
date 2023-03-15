ac6ca760bf52a0aef837b7f304d9e8008acbac5a
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-15 08:52:47 +0300

    add task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,15 +10,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int mask = 0x01;
 
-	max <<= index;
+	mask <<= index;
 
-	if (max == 0 || index > 64)
+	if (mask == 0 || index > 64)
 		return (-1);
-	if ((n & max))
+	if ((n & mask))
 		return (1);
-	else
-		return (0);
+	return (0);
 }
 
