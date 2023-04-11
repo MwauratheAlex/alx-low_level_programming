7847459d3fa06d7bb0d3146f72353a5e89848923
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-11 10:57:46 +0300

    add 0-linear.c

---
+++ 0x1E-search_algorithms/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    int *array = NULL;
+    size_t size = 0;
+
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return (EXIT_SUCCESS);
+}
